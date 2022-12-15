7780e8fecba2b9bff6ea78ebfdc7bb93bdf1d47b
khalil <khilel208@gmail.com>
2022-12-15 09:01:07 +0300

    Add project files.

---
+++ Api/Controllers/StudentController.cs
@@ -0,0 +1,115 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public StudentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"Select StudentId, FullName, Class from dbo.Student";
+            DataTable table = new DataTable();
+            string? sqlDataSource = _configuration.GetConnectionString("StudentAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+
+        [HttpPost]
+        public JsonResult Post(Student objStudent)
+        {
+            string query = @"Insert into dbo.Student values
+                ('" + objStudent.FullName + "','" + objStudent.Class + "')";
+            DataTable table = new DataTable();
+            string? sqlDataSource = _configuration.GetConnectionString("StudentAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Student objStudent)
+        {
+            string query = @"Update dbo.Student set
+                FullName = '" + objStudent.FullName + @"',
+                Class='" + objStudent.Class + "' where StudentId = " + objStudent.StudentId;
+            DataTable table = new DataTable();
+            string? sqlDataSource = _configuration.GetConnectionString("StudentAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"Delete from dbo.Student where StudentId = " + id;
+            DataTable table = new DataTable();
+            string? sqlDataSource = _configuration.GetConnectionString("StudentAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
---
+++ Api/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Models
+{
+    public class Student
+    {
+
+        public int StudentId { get; set; }
+        public string? FullName { get; set; }
+        public string? Class { get; set; }
+    }
+}
